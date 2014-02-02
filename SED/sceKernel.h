a8f24e201206c64177dbe18826f8c8f75abab59c
hgoel0974 <hgoel0974@gmail.com>
2014-02-02 02:19:10 -0400

    Initial Commit

---
+++ SED/sceKernel.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CommonTypes.h"
+
+typedef int SceUID;
+typedef unsigned int SceSize;
+typedef int SceSSize;
+typedef unsigned char SceUChar;
+typedef unsigned int SceUInt;
+typedef int SceMode;
+typedef s64 SceOff;
+typedef u64 SceIores;
+
+typedef u32 u32_le;
+typedef u16 u16_le;
+typedef u64 u64_le;
+
+typedef s32 s32_le;
+typedef s16 s16_le;
+typedef s64 s64_le;
+
+typedef float float_le;
+typedef double double_le;
+
+typedef s32_le SceUID_le;
+typedef u32_le SceSize_le;
+typedef s32_le SceSSize_le;
+typedef u32_le SceUInt_le;
+typedef s32_le SceMode_le;
+typedef s64_le SceOff_le;
+typedef s64_le SceIores_le;
